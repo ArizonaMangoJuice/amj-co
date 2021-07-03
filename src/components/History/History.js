0b9acdaf8f3393845e116193e50dc043a1516b7d
isael lizama <isaellizama@isaels-MacBook-Air.local>
2021-07-03 08:02:35 -0600

    added styling to call to action button

--- src/components/History/History.js
+++ src/components/History/History.js
@@ -8,8 +8,11 @@ const History = props => {
                 <img className='image-2 history-image-block' src='https://images.unsplash.com/photo-1505855796860-aa05646cbf1f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1500&q=80'/>
             </section>
             <section className='history-text'>
-                <h1 className='history-h1'>Velit velit voluptate nostrud proident sunt enim pariatur in.</h1>
+                <h1 className='history-h1'>We Got This</h1>
                 <p className='history-p'>Laboris in non nostrud pariatur. Deserunt pariatur quis exercitation cillum duis. Ad non in voluptate consectetur dolor et voluptate aliqua irure. Ut aliqua qui laboris proident do cupidatat cupidatat mollit dolor et non nulla quis esse.</p>
+                <section className='history-cta'>
+                    <button className='history-button'>Make An Appointment!</button>
+                </section>
             </section>
         </section>
     )
